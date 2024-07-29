273795497b5cae26e0bab350033ea36cf44e45a8
Ryan MacGillivray <rmacg89@gmail.com>
2024-07-29 15:21:10 +0100

    Add basic income and expenditure total methods
    We know we'll need this both to calculate the I&E rating and to use for
    presentational methods. This does the totalling and adds a basic test
    for each method.

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -5,4 +5,12 @@ class Statement < ApplicationRecord
   validates :user, presence: true
   validates :incomes, presence: true
   validates :expenditures, presence: true
+
+  def total_income
+    incomes.sum(&:amount)
+  end
+
+  def total_expenditure
+    expenditures.sum(&:amount)
+  end
 end
--- test/models/statement_test.rb
+++ test/models/statement_test.rb
@@ -20,4 +20,24 @@ class StatementTest < ActiveSupport::TestCase
     statement = build(:statement, :with_income)
     assert statement.invalid?
   end
+
+  test '#total_income' do
+    statement = build(:statement, :with_expenditure)
+    income_1 = create(:income, statement:)
+    income_2 = create(:income, name: 'Investments', amount: 2000.00, statement:)
+    statement.incomes << [income_1, income_2]
+    statement.save
+
+    assert_equal 3000.00, statement.reload.total_income
+  end
+
+  test '#total_expenditure' do
+    statement = build(:statement, :with_income)
+    expenditure_1 = create(:expenditure, statement:)
+    expenditure_2 = create(:expenditure, name: 'Fees', amount: 10.10, statement:)
+    statement.expenditures << [expenditure_1, expenditure_2]
+    statement.save
+
+    assert_equal 20.09, statement.reload.total_expenditure
+  end
 end
