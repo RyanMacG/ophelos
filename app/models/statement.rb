9066109900d15620183e865889f5dfbc865c8b73
Ryan MacGillivray <rmacg89@gmail.com>
2024-07-29 15:54:50 +0100

    Add #income_expenditure_rating calculation
    We want to be able to return a rating depending on the ratio of
    expenditure vs income. This adds the base calculations and returns a
    letter rating as appropriate.

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -17,4 +17,14 @@ class Statement < ApplicationRecord
   def disposable_income
     total_income - total_expenditure
   end
+
+  def income_expenditure_rating
+    ratio = total_expenditure / total_income
+    case ratio
+    when 0...0.1 then 'A'
+    when 0.1...0.3 then 'B'
+    when 0.3...0.5 then 'C'
+    else 'D'
+    end
+  end
 end
